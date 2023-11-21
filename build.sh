9fee4a0a7ff7b85b931908f41162375a8aa45567
pedroazeredo04 <pedro_azeredo@usp.br>
2023-11-21 11:06:06 -0300

    ⚗️ Try to fix build script

--- build.sh
+++ build.sh
@@ -15,6 +15,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-User.objects.filter(username=os.environ["pedrin_dj"]).exists() or \
-    User.objects.create_superuser(os.environ["pedrin_dj"], os.environ["pedro_azeredo@usp.br"], os.environ["cavalobranco123"])
+# Verificar se as chaves do ambiente estão definidas antes de acessá-las
+username = os.environ.get("pedrin_dj")
+email = os.environ.get("pedro_azeredo@usp.br")
+password = os.environ.get("cavalobranco123")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
 EOF
