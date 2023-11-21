1f809bf016b4cbee9d2cb5cff0c6cc4ea99e94ef
pedroazeredo04 <pedro_azeredo@usp.br>
2023-11-21 10:54:50 -0300

    Adiciona views com as classes genéricas ListView, DetailView, CreateView, UpdateView e DeleteView;

--- posts/urls.py
+++ posts/urls.py
@@ -4,12 +4,10 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.list_posts, name='index'),
-    path('<int:post_id>/', views.detail_post, name='detail'),
-    
+    path('', views.PostsListView.as_view(), name='index'),
+    path('create/', views.PostsCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', views.PostsUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.PostsDeleteView.as_view(), name='delete'),
+    path('<int:pk>/', views.PostsDetailView.as_view(), name='detail'),
     path('search/', views.search_posts, name='search'),
-    path('create/', views.create_post, name='create'),
-
-    path('update/<int:post_id>/', views.update_post, name='update'),
-    path('delete/<int:post_id>/', views.delete_post, name='delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,36 +3,16 @@ from django.urls import reverse
 from .models import Posts
 from .forms import PostsForm
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
+class PostsListView(generic.ListView):
+    model = Posts
+    template_name = 'posts/index.html'
 
 
-def list_posts(request):
-    post_data = Posts.objects.all()
-    context = {"posts_list": post_data}  ## Dicionário com a lista de posts
-    return render(request, 'posts/index.html', context)
-
-
-def detail_post(request, post_id):
-    post = get_object_or_404(Posts, pk=post_id)
-    context = {'post': post}
-    return render(request, 'posts/detail.html', context)
-
-
-def create_post(request):
-    if request.method == 'POST':
-        form = PostsForm(request.POST)
-        if form.is_valid():
-            requested_post_name = form.cleaned_data['title']
-            requested_post_text = form.cleaned_data['text']
-            post = Posts(title=requested_post_name,
-                        text=requested_post_text)
-            post.save()
-            return HttpResponseRedirect(
-                reverse('posts:detail', args=(post.id, )))
-    else:
-        form = PostsForm()
-    context = {'form': form}
-    return render(request, 'posts/create.html', context)
-
+class PostsDetailView(generic.DetailView):
+    model = Posts
+    template_name = 'posts/detail.html'
 
 def search_posts(request):
     context = {}
@@ -43,38 +23,25 @@ def search_posts(request):
     
     return render(request, 'posts/search.html', context)
 
+class PostsCreateView(generic.CreateView):
+    model = Posts
+    template_name = 'posts/create.html'
+    form_class = PostsForm
 
-def update_post(request, post_id):
-    post = get_object_or_404(Posts, pk=post_id)
+    def get_success_url(self) -> str:
+        return reverse('posts:detail', args=(self.object.id, ))
 
-    if request.method == "POST":
-        form = PostsForm(request.POST)
-        if form.is_valid():
-            post.title = form.cleaned_data['title']
-            post.text = form.cleaned_data['text']
-            post.save()
-            return HttpResponseRedirect(
-                reverse('posts:detail', args=(post.id, ))
-            )
+class PostsUpdateView(generic.UpdateView):
+    model = Posts
+    template_name = 'posts/update.html'
+    form_class = PostsForm
 
-    else:
-        form = PostsForm(
-            initial={
-                'title': post.title,
-                'text': post.text
-            }
-        )
+    def get_success_url(self) -> str:
+        return reverse('posts:detail', args=(self.object.id, ))
 
-    context = {'post': post, 'form': form}
-    return render(request, 'posts/update.html', context)
+class PostsDeleteView(generic.DeleteView):
+    model = Posts
+    template_name = 'posts/delete.html'
 
-
-def delete_post(request, post_id):
-    post = get_object_or_404(Posts, pk=post_id)
-
-    if request.method == "POST":
-        post.delete()
-        return HttpResponseRedirect(reverse('posts:index'))
-
-    context = {'post': post}
-    return render(request, 'posts/delete.html', context)
+    def get_success_url(self) -> str:
+        return reverse('posts:index')
